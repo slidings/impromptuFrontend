df221e844d964ed0f898f609cbdd34f1d4a2923a
slye20 <samuellim2011@gmail.com>
2024-06-30 22:10:19 +0800

    paths

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,17 @@ import EditJobPage from "./pages/EditJobPage";
 import LogIn from "./pages/LogInPage";
 import SignUp from "./pages/SignUpPage";
 import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ children, loggedIn }) {
+  if (!loggedIn) {
+    // If the user is not logged in, redirect to the login page
+    return <Navigate to="/login" replace />;
+  }
+
+  // If the user is logged in, render the children components
+  return children;
+}
 
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(!!localStorage.getItem("token"));
@@ -44,33 +55,42 @@ const App = () => {
         path="/"
         element={<MainLayout loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
       >
-        {!loggedIn && (
-          <Route path="/" element={<LogIn setLoggedIn={setLoggedIn} />} />
-        )}
-        {loggedIn && (
-          <>
-            <Route index element={<HomePage />} />
-            <Route path="/jobs" element={<JobsPage />} />
-            <Route path="/add-job" element={<AddJobPage />} />
-            <Route
-              path="/edit-job/:id"
-              element={<EditJobPage updateJobSubmit={updateJob} />}
-              loader={jobLoader}
-            />
-            <Route
-              path="/jobs/:id"
-              element={<JobPage deleteJob={deleteJob} />}
-              loader={jobLoader}
-            />
-          </>
-        )}
+        <>
+          <Route index element={<HomePage />} />
+          <Route path="/jobs" element={<JobsPage />} />
+          <Route
+            path="/add-job"
+            element={
+              <ProtectedRoute loggedIn={loggedIn}>
+                <AddJobPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/edit-job/:id"
+            element={
+              <ProtectedRoute loggedIn={loggedIn}>
+                <EditJobPage updateJobSubmit={updateJob} />
+              </ProtectedRoute>
+            }
+            loader={jobLoader}
+          />
+          <Route
+            path="/jobs/:id"
+            element={
+              <ProtectedRoute loggedIn={loggedIn}>
+                <JobPage deleteJob={deleteJob} />
+              </ProtectedRoute>
+            }
+            loader={jobLoader}
+          />
+        </>
         <Route path="/login" element={<LogIn setLoggedIn={setLoggedIn} />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
   );
-
   return <RouterProvider router={router} />;
 };
 
